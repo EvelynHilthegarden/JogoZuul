5c97e7daed0a85def53abd8c22bf5dfb814d9ffb
EvelynHilthegarden <142934215+EvelynHilthegarden@users.noreply.github.com>
2023-08-31 16:05:23 -0300

    Game.java

--- Game.java
+++ Game.java
@@ -1,25 +1,9 @@
-/**
- *  This class is the main class of the "World of Zuul" application. 
- *  "World of Zuul" is a very simple, text based adventure game.  Users 
- *  can walk around some scenery. That's all. It should really be extended 
- *  to make it more interesting!
- * 
- *  To play this game, create an instance of this class and call the "play"
- *  method.
- * 
- *  This main class creates and initialises all the others: it creates all
- *  rooms, creates the parser and starts the game.  It also evaluates and
- *  executes the commands that the parser returns.
- * 
- * @author  Michael Kölling and David J. Barnes
- * @version 2016.02.29
- */
-
 public class Game 
 {
     private Parser parser;
     private Room currentRoom;
-        
+    private Room previousRoom;
+
     /**
      * Create the game and initialise its internal map.
      */
@@ -35,7 +19,7 @@ public class Game
     private void createRooms()
     {
         Room outside, theater, pub, lab, office, meuQuarto, minhaSala, minhaCozinha;
-
+      
         // create the rooms
         outside = new Room("outside the main entrance of the university");
         theater = new Room("in a lecture theater");
@@ -46,14 +30,27 @@ public class Game
         minhaSala = new Room("na sala mais confortável da universidade");
         minhaCozinha = new Room("na cozinha com comidas mais gostosas da universidade");
 
+        // put items in the room
+        outside.addItem(new Item("árvores", 0.5));
+        outside.addItem(new Item("pessoas", 55));
+        pub.addItem(new Item("vinho", 0.75));
+        lab.addItem(new Item("computador", 30));
+        lab.addItem(new Item("mouse", 0.6));
+        theater.addItem(new Item("atores", 50));
+        office.addItem(new Item("folhas", 0.1));
+        meuQuarto.addItem(new Item("minha cama", 30));
+        minhaSala.addItem(new Item("meu sofá", 50));
+        minhaCozinha.addItem(new Item("meu fogão", 26));
+        
         // initialise room exits
+        outside.setExit("north", meuQuarto);
         outside.setExit("east", theater);
         outside.setExit("south", lab);
         outside.setExit("west", pub);
 
         theater.setExit("west", outside);
 
-        pub.setExit("east", outside);
+        pub.setExit("east", theater);
 
         lab.setExit("north", outside);
         lab.setExit("east", office);
@@ -61,9 +58,12 @@ public class Game
         office.setExit("west", lab);
 
         meuQuarto.setExit("north", minhaSala);
-        minhaSala.setExit("east", minhaCozinha);
-        minhaCozinha.setExit("west", minhaSala);
+        meuQuarto.setExit("south", outside);
 
+        minhaSala.setExit("south", meuQuarto);
+        minhaSala.setExit("west", minhaCozinha);
+
+        minhaCozinha.setExit("east", minhaSala);
 
         currentRoom = outside;  // start game outside
     }
@@ -86,6 +86,7 @@ public class Game
         System.out.println("Thank you for playing.  Good bye.");
     }
 
+    /*
     /**
      * Print out the opening message for the player.
      */
@@ -120,10 +121,19 @@ public class Game
         else if (commandWord.equals("go")) {
             goRoom(command);
         }
+        else if (commandWord.equals("look")) {
+            look();
+        }
+        else if (commandWord.equals("eat")) {
+            eat();
+        }
+        else if (commandWord.equals("back")) {
+            goBack(command);
+        }
         else if (commandWord.equals("quit")) {
             wantToQuit = quit(command);
         }
-        // else command not recognised.
+
         return wantToQuit;
     }
 
@@ -140,12 +150,22 @@ public class Game
         System.out.println("around at the university.");
         System.out.println();
         System.out.println("Your command words are:");
-        parser.showCommands();
+        System.out.println(parser.getCommandList());
+    }
+
+    /**
+     * Enters the specified room and prints the description.
+     */
+    private void enterRoom(Room nextRoom)
+    {
+        previousRoom = currentRoom;
+        currentRoom = nextRoom;
+        System.out.println(currentRoom.getLongDescription());
     }
 
     /** 
-     * Try to in to one direction. If there is an exit, enter the new
-     * room, otherwise print an error message.
+     * Try to go in one direction. If there is an exit, enter
+     * the new room, otherwise print an error message.
      */
     private void goRoom(Command command) 
     {
@@ -164,9 +184,36 @@ public class Game
             System.out.println("There is no door!");
         }
         else {
-            currentRoom = nextRoom;
-            System.out.println(currentRoom.getLongDescription());
+            enterRoom(nextRoom);
         }
+    }
+
+    /**
+     * Go back to the previous room.
+     */
+    private void goBack(Command command){
+        if(command.hasSecondWord()) {
+            System.out.println("Back where?");
+            return;
+        }
+        if (previousRoom == null) {
+            System.out.println("You have nowhere to go back to!");
+        }
+        else {
+            enterRoom(previousRoom);
+        }
+    }
+
+    private void look(){
+        System.out.println(currentRoom.getLongDescription());
+    }
+
+    private void eat(){
+        System.out.println("Você já comeu e está de barriga cheia!");
+    }
+
+    private void back(){
+
     }
 
     /** 
