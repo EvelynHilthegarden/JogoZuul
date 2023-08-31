91b8db6323f9d99b60bd741f514a6d3124bd7d9a
EvelynHilthegarden <142934215+EvelynHilthegarden@users.noreply.github.com>
2023-08-31 16:06:31 -0300

    Parser.java

--- Parser.java
+++ Parser.java
@@ -1,21 +1,5 @@
 import java.util.Scanner;
 
-/**
- * This class is part of the "World of Zuul" application. 
- * "World of Zuul" is a very simple, text based adventure game.  
- * 
- * This parser reads user input and tries to interpret it as an "Adventure"
- * command. Every time it is called it reads a line from the terminal and
- * tries to interpret the line as a two word command. It returns the command
- * as an object of class Command.
- *
- * The parser has a set of known command words. It checks user input against
- * the known commands, and if the input is not one of the known commands, it
- * returns a command object that is marked as an unknown command.
- * 
- * @author  Michael Kölling and David J. Barnes
- * @version 2016.02.29
- */
 public class Parser 
 {
     private CommandWords commands;  // holds all valid command words
@@ -28,6 +12,11 @@ public class Parser
     {
         commands = new CommandWords();
         reader = new Scanner(System.in);
+    }
+
+    //Imprime uma lista de palavras de comando válidos
+    public String getCommandList(){
+        return commands.getCommandList();
     }
 
     /**
@@ -62,12 +51,5 @@ public class Parser
             return new Command(null, word2); 
         }
     }
-
-    /**
-     * Print out a list of valid command words.
-     */
-    public void showCommands()
-    {
-        commands.showAll();
-    }
 }
+
